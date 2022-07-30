70b6d8a4d57fa466b9747fa5d744bb369fcb8765
mdshoheb <qamdshoheb@gmail.com>
2022-07-30 14:08:48 -0400

    07302022 2:08PM

--- src/hw05Q2Constructor/ComputerTest.java
+++ src/hw05Q2Constructor/ComputerTest.java
@@ -5,7 +5,7 @@ public class ComputerTest {
 	public static void main(String[] args) {
 		Computer computer = new Computer();
 		Computer computer1 = new Computer("Apple", "MacBook Air", "MacOs Mojave", 1500, 'A', false);
-		Computer computer2 = new Computer("HP", "Windows", "Pavilion", 'B', 599, true);
+		Computer computer2 = new Computer("HP", "Windows Os", "Pavilion", 'B', 599, true);
 
 	}
 
