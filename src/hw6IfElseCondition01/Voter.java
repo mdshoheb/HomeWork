dd30fcf6c71631a76f6d0e91b6633ef3c5e871d9
mdshoheb <qamdshoheb@gmail.com>
2022-08-07 17:35:25 -0400

    08072022 11:30PM

---
+++ src/hw6IfElseCondition01/Voter.java
@@ -0,0 +1,21 @@
+package hw6IfElseCondition01;
+
+public class Voter {
+
+	public static void main(String[] args) {
+		int age = 30;
+
+		if (age == 18) {
+			System.out.println("I am a voter");
+		} else if (age < 18) {
+			System.out.println("I am not a voter");
+
+		} else if (age > 18) {
+			System.out.println("I am a voter");
+		} else {
+			System.out.println("Please add a valid age");
+		}
+
+	}
+
+}
