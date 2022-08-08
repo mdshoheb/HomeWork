1effe5e62739d9cfcfe81565accbeec02afc19e6
mdshoheb <qamdshoheb@gmail.com>
2022-08-08 11:29:39 -0400

    08082022 11:30Am

--- src/hw6IfElseCondition01/Voter.java
+++ src/hw6IfElseCondition01/Voter.java
@@ -3,7 +3,7 @@ package hw6IfElseCondition01;
 public class Voter {
 
 	public static void main(String[] args) {
-		int age = 30;
+		int age = 100;
 
 		if (age == 18) {
 			System.out.println("I am a voter");
---
+++ src/hw6IfElseCondition02/Temperature.java
@@ -0,0 +1,30 @@
+package hw6IfElseCondition02;
+
+import java.util.Scanner;
+
+public class Temperature {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter today's temperature here:");
+		Scanner scanner = new Scanner(System.in);
+		int todaysTemperature = scanner.nextInt();
+
+		if (todaysTemperature < 32) {
+			System.out.println("Freezing");
+
+		} else if (todaysTemperature < 55) {
+			System.out.println("Pleasant");
+
+		} else if (todaysTemperature < 73) {
+			System.out.println("Getting Warmer");
+
+		} else if (todaysTemperature < 101) {
+			System.out.println("Hot");
+
+		} else {
+			System.out.println("Ice on my head please");
+		}
+
+	}
+
+}
