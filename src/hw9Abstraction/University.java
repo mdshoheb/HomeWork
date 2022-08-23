9098a53ff60193ff299547cdde83e52dcf155107
mdshoheb <qamdshoheb@gmail.com>
2022-08-22 22:57:20 -0400

    123

--- src/hw9Abstraction/University.java
+++ src/hw9Abstraction/University.java
@@ -11,12 +11,16 @@ public interface University extends College, Hospital {
 //	}
 
 	public void classSize();
-    public void playGround();
-    public void teacher();
-    public default void gymnasium() {
+
+	public void playGround();
+
+	public void teacher();
+
+	public default void gymnasium() {
 		System.out.println("This default method gymnasium from University Interface");
 	}
-    public static void library() {
+
+	public static void library() {
 		System.out.println("This static method library from University Interface");
 	}
 
