77293e2236165af18e1b5fa6fcd92ebd58aeea96
mdshoheb <qamdshoheb@gmail.com>
2022-08-23 11:59:09 -0400

    08232022 11:59AM

--- src/hw10Abstraction/University.java
+++ src/hw10Abstraction/University.java
@@ -1,9 +1,10 @@
 package hw10Abstraction;
 
 /*
- 1. One keyword "extends" is used to inherit an Interface.
+ 1.One keyword "extends" is used to inherit an Interface.
  2.An Interface can not inherit a regular class or an abstract class by extends or implements keywords.
- 3. An Interface can inherit more than one Interface by extends keyword.
+ 3.An Interface can not inherit another interface by implements keyword.
+ 3.An Interface can inherit more than one Interface by extends keyword.
  */
 
 public interface University extends College, Hospital {
--- src/hw9Abstraction/University.java
+++ src/hw9Abstraction/University.java
@@ -1,9 +1,9 @@
 package hw9Abstraction;
 
 /*
- 1. One keyword "extends" is used to inherit an Interface.
+ 1.One keyword "extends" is used to inherit an Interface.
  2.An Interface can not inherit a regular class or an abstract class by extends keyword.
- 3. An Interface can inherit more than one Interface by extends keyword.
+ 3.An Interface can inherit more than one Interface by extends keyword.
  */
 
 public interface University extends College, Hospital {
