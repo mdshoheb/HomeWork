46abd999b45afecfeb94b8d1a2267e835faf717a
mdshoheb <qamdshoheb@gmail.com>
2022-09-14 12:54:31 -0400

    09142022 12:54PM

---
+++ src/hw19/Xpath.java
@@ -0,0 +1,58 @@
+package hw19;
+
+public class Xpath {
+	/*
+	  1. Tag with a single attribute.
+	  Source: https://www.target.com/
+	  
+	  a.//input[@id='search']
+	  b.//a[@id='utilityNav-redCardLI']
+	  c.//button[@class='styles__SearchButton-sc-srf2ow-7 cgYZJJ']
+	  d//a[@id='utilityNav-weeklyAd']
+	  e.//a[@id='utilityNav-circle']
+	  
+	  2.Tag with multiple attribute with 'and' logic
+	  Source:https://www.capitalone.com/
+	  
+	  a.//input[@id='ods-input-0' and @name='ods-input-0']
+	  b.//button[@name='noAcctSubmit' and @class='submit-btn grv-button--progressive grv-button--compact']
+	  c.//a[@id='setUpAccess' and @class='asset-link set-up-access grv-text__body--tiny']
+	  d.//a[@id='unav-l1-locations' and @class='site-header__navlink site-utility-nav__locations ng-star-inserted']
+	  e.//button[@id='unav-l1-search' and @ class='site-header__navlink site-utility-nav__search ng-star-inserted']
+	  
+	  3. Tag with inner text 
+	  Source:https://www.capitalone.com/
+	  
+	  a.//span[text()='Checking & Savings']
+	  b.//span[text()='Learn & Grow']
+	  c.//span[text()='Credit Cards']
+	  d.//h2[text()='Virtual Cards with Google Pay']
+	  e.//div[text()='Easily create and use virtual cards directly on Google Chrome.']
+	  
+	  4. Tag with inner text [not complete inner text rather partial text as well as actual text]
+	  Source:https://www.capitalone.com/
+	  
+	  a.//p[contains(text(),'Pre-qualify to see')]
+	  b.//h2[contains(text(), 'No impact, no worries')] 
+	  c.//p[contains(text(),'Automatically get bett')]
+	  d.//span[contains(text(), 'sleep better at night')] 
+	  e.//div[contains(text(), 'so that I c')] 
+	  
+	  5. Tag with attributes and use of contains.
+	  Source:https://www.capitalone.com/
+	  
+	  a.//button[contains(@class,'submit-btn grv-butto')]
+	  b.//input[contains(@name,'ods-input-0')]
+	  c.//p[contains(@class,'login-auth-')]
+	  d.//h2[contains(@class,'hero-card__headline g')]
+	  e.//a[contains(@class,'grv-button--pr')]
+	 */
+}
+	
+	
+	
+	  
+
+	  
+	  
+	
